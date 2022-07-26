a76d7c8066da1dba98761c1ae49e4ce26ec48bd8
Aaryaman Maity <aaryamanmaity@gmail.com>
2022-07-26 11:49:52 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
+// import HigherOrderFunctions from './components/hof/HigherOrderFunctions';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Main extends React.Component {
+
+  constructor(props) {
+    super(props);
+    // to bind the method with event handler without (). 
+    // this.handleChange = this.handleChange.bind(this);
+
+    this.state =
+    {
+      value: ''
+    };
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
+  render() {
+    return <>
+      <div className="container">
+        <div className="input">
+          <h3>Input</h3>
+          <textarea className="input-text" onChange={(event)=> this.handleChange(event)} defaultValue={this.state.value} />
+        </div>
+        <div className="output">
+          <h3>Pro Note</h3>
+          <div className="output-text">{this.state.value}</div>
+        </div>
+      </div>
+    </>
+  }
 }
 
-export default App;
+export default Main
